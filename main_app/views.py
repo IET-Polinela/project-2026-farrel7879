d9d84e9f525e963680920ee1dcbeb78b2f689d63
Farrel <emailgithublu@gmail.com>
2026-04-01 19:48:50 +0700

    lab3

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Report
+from .forms import ReportForm
+
+def add_report(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ReportForm()
+    return render(request, 'main_app/add_report.html', {'form': form})
 
 def home(request):
-    return render(request, 'main_app/home.html')
+    reports = Report.objects.all()
+    return render(request, 'main_app/home.html', {'reports': reports})
+
+def delete_report(request, id):
+    report = Report.objects.get(id=id)
+    report.delete()
+    return redirect('home')
+
+def update_report(request, id):
+    report = Report.objects.get(id=id)
+    if request.method == "POST":
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ReportForm(instance=report)
+    return render(request, 'main_app/update_report.html', {'form': form})
